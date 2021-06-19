884b6c9048055eac872c1b2ff52ee8000f3f6053
Verisana <account@0112.pro>
2021-06-19 20:43:50 +0500

    feat: :sparkles: create IRebalancer interface draft

---
+++ contracts/interfaces/IRebalancer.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.4;
+
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+interface IRebalancer {
+    function factory() external view returns (address);
+    function pool() external view returns (IUniswapV3Pool);
+
+    function rebalancePriceRange() external;
+
+    function removeLiquidityStake() external;
+
+    function immediateReturnFunds() external;
+
+    function collectAllFees() external;
+
+    function sendClaimedFunds() external;
+
+    function mergeStakes() external;
+
+    function summarizeTrades() external;
+}
