a1770e48516b3d9bb3a0ee59e31a692edd16ace2
Verisana <account@0112.pro>
2021-06-19 20:41:56 +0500

    feat: :sparkles: create RebelancerDeployer contract. Not tested

---
+++ contracts/RebalancerDeployer.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.4;
+
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "./interfaces/IRebalancerDeployer.sol";
+import "./Rebalancer.sol";
+
+contract RebalancerDeployer is IRebalancerDeployer {
+    struct Parameters {
+        address factory;
+        IUniswapV3Pool pool;
+    }
+
+    Parameters public override parameters;
+
+    function deploy(address factory, IUniswapV3Pool pool)
+        internal
+        returns (address rebalancer)
+    {
+        parameters = Parameters({factory: factory, pool: pool});
+        rebalancer = address(
+            new Rebalancer{salt: keccak256(abi.encode(pool))}()
+        );
+        delete parameters;
+    }
+}
