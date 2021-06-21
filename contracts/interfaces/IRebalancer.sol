6692eee9304a7f843be3584968fb789b71e0fd10
Verisana <account@0112.pro>
2021-06-21 22:08:21 +0500

    feat: :sparkles: add positionManager state

--- contracts/interfaces/IRebalancer.sol
+++ contracts/interfaces/IRebalancer.sol
@@ -2,16 +2,21 @@
 pragma solidity ^0.8.4;
 
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
 
 interface IRebalancer {
+
+    
+
     function factory() external view returns (address);
     function pool() external view returns (IUniswapV3Pool);
+    function positionManager() external view returns (INonfungiblePositionManager);
 
     function rebalancePriceRange() external;
 
     function removeLiquidityStake() external;
 
-    function immediateReturnFunds() external;
+    function immediateFundsReturn() external;
 
     function collectAllFees() external;
 
