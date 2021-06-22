6cdbcabd24d432e9685bbb1ddeb61c9808716808
Verisana <account@0112.pro>
2021-06-22 21:43:51 +0500

    feat: :sparkles: make contract compilable (wip)

--- contracts/Rebalancer.sol
+++ contracts/Rebalancer.sol
@@ -14,15 +14,16 @@ import "./NoDelegateCall.sol";
 contract Rebalancer is Ownable, NoDelegateCall {
     using SafeERC20 for IERC20;
 
-    address public immutable override factory;
-    IUniswapV3Pool public immutable override pool;
-    uint256 public immutable override shareDenominator = 1000000;
-    INonfungiblePositionManager public immutable override positionManager =
+    address public immutable factory;
+    IUniswapV3Pool public immutable pool;
+    uint256 public immutable shareDenominator = 1000000;
+    INonfungiblePositionManager public immutable positionManager =
         INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
 
-    uint public override totalGasUsed = 0;
+    uint256 public totalGasUsed = 0;
+    uint256 public lastBlockSummirized = 0;
 
-    struct NFTPosition {
+    struct Position {
         uint256 tokenId;
         uint128 liquidity;
         uint256 amount0;
@@ -31,39 +32,34 @@ contract Rebalancer is Ownable, NoDelegateCall {
         int24 tickUpper;
     }
 
-    struct FundsAllocated {
-        mapping(address => uint256) share;
+    struct Funds {
         address[] users;
-    }
-
-    struct FundsToAllocate {
-        mapping(address => uint256) token0Amount;
-        mapping(address => uint256) token1Amount;
         uint256 total0Amount;
         uint256 total1Amount;
-        address[] users;
+        mapping(address => uint256) share;
+        mapping(address => uint256) token0Amount;
+        mapping(address => uint256) token1Amount;
     }
 
-    struct FundsToRefund {
-        address[] users;
-    }
-
-    struct AccountedFees {
+    struct Fees {
         uint256 totalToken0Fee;
         uint256 totalToken1Fee;
     }
 
-    AccountedFees accountedFees = AccountedFees(0, 0);
-    NFTPosition public override position = NFTPosition(0, 0, 0, 0, 0, 0);
+    Fees feesIncome = Fees(0, 0);
+    Funds public fundsAllocated;
+    Funds public fundstoAllocate;
+    address[] withdrawals;
+    Position public openPosition = Position(0, 0, 0, 0, 0, 0);
 
     modifier validateSharesCalculation() {
         _;
     }
 
     modifier saveGasConsumption() {
-        uint gasStart = gasleft();
+        uint256 gasStart = gasleft();
         _;
-        uint gasUsed = gasStart - gasleft();
+        uint256 gasUsed = gasStart - gasleft();
         totalGasUsed += gasUsed * tx.gasprice;
     }
 
@@ -75,53 +71,72 @@ contract Rebalancer is Ownable, NoDelegateCall {
 
     function rebalancePriceRange(int24 tickLowerCount, int24 tickUpperCount)
         external
-        override
         onlyOwner
     {
-        if (position.tokenId == 0) {
-            openNewPosition(tickLowerCount, tickUpperCount);
+        if (openPosition.tokenId == 0) {
+            _openNewPosition(tickLowerCount, tickUpperCount);
         } else {
-            collectFees();
-            removeLiquidityStake();
-            openNewPosition(tickLowerCount, tickUpperCount);
+            // collectFees();
+            // removeLiquidityStake();
+            _openNewPosition(tickLowerCount, tickUpperCount);
         }
     }
 
-    function allocateNewFunds(uint256 token0Amount, uint256 token1Amount)
-        external
-        override
+    function _openNewPosition(int24 tickLowerCount, int24 tickUpperCount)
+        private
     {
+        // (
+        //     uint256 tokenId,
+        //     uint128 liquidity,
+        //     uint256 amount0,
+        //     uint256 amount1
+        // ) = positionManager.mint({
+        //     token0: pool.token0(),
+        //     token1: pool.token1(),
+        //     fee: pool.fee(),
+        //     tickLower: 0,
+        //     tickUpper: 0,
+        //     amount0Desired: 0,
+        //     amount1Desired: 0,
+        //     amount0Min: 0,
+        //     amount1Min: 0,
+        //     recipient: address(this),
+        //     deadline: 0
+        // });
+    }
+
+    function addNewFunds(uint256 token0Amount, uint256 token1Amount) external {
         require(
             token0Amount > 0 || token1Amount > 0,
             "Either token0Amount or token1Amount should be greater than 0"
         );
 
-        IERC20 token0 = IERC20(pool.token0);
+        IERC20 token0 = IERC20(pool.token0());
         require(
             token0.allowance(msg.sender, address(this)) >= token0Amount,
             "Not enough allowance of token0Amount to execute allocating"
         );
 
-        IERC20 token1 = IERC20(pool.token1);
+        IERC20 token1 = IERC20(pool.token1());
         require(
             token1.allowance(msg.sender, address(this)) >= token1Amount,
             "Not enough allowance of token1Amount to execute allocating"
         );
     }
 
-    function removeLiquidityStake() external override {}
+    function removeLiquidityStake() external {}
 
-    function immediateFundsReturn() external override {}
+    function immediateFundsReturn() external {}
 
-    function collectFees() external override {
-        positionManager.collect();
+    function collectFees() external {
+        // positionManager.collect();
     }
 
-    function divideFees() internal override {}
+    function divideFees() internal {}
 
-    function sendClaimedFunds() external override {}
+    function sendClaimedFunds() external {}
 
-    function mergeStakes() external override {}
+    function mergeStakes() external {}
 
-    function summarizeTrades() external override {}
+    function summarizeTrades() external {}
 }
