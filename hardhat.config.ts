406c4d6ade207a54609b9924981744465fe983c4
Verisana <account@0112.pro>
2021-06-23 22:15:01 +0500

    build: :wrench: remove all unnecessary compilers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,15 +26,6 @@ export default {
                     }
                 }
             }
-            // {
-            //     version: "0.7.6",
-            //     settings: {
-            //         optimizer: {
-            //             enabled: true,
-            //             runs: 1000
-            //         }
-            //     }
-            // }
         ]
     },
     paths: {
