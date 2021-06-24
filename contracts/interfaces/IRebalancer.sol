1047fe4dba89120e4985c73f94dbb69414d1ff98
Verisana <account@0112.pro>
2021-06-24 22:08:45 +0500

    feat: :sparkles: mock almost all functionality

--- contracts/interfaces/IRebalancer.sol
+++ contracts/interfaces/IRebalancer.sol
@@ -5,24 +5,14 @@ import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
 
 interface IRebalancer {
-
-    
-
-    function factory() external view returns (address);
-    function pool() external view returns (IUniswapV3Pool);
-    function positionManager() external view returns (INonfungiblePositionManager);
-
-    function rebalancePriceRange() external;
-
-    function removeLiquidityStake() external;
-
+    // function factory() external view returns (address);
+    // function pool() external view returns (IUniswapV3Pool);
+    // function positionManager() external view returns (INonfungiblePositionManager);
+    // function rebalancePriceRange() external;
+    // function removeLiquidityStake() external;
     function immediateFundsReturn() external;
-
-    function collectAllFees() external;
-
-    function sendClaimedFunds() external;
-
-    function mergeStakes() external;
-
-    function summarizeTrades() external;
+    // function collectAllFees() external;
+    // function sendClaimedFunds() external;
+    // function mergeStakes() external;
+    // function summarizeTrades() external;
 }
