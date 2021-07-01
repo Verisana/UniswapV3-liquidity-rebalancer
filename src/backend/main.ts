c394f2866060aa77e4dcd7e76041c4a4ce4ee2c2
Verisana <account@0112.pro>
2021-07-01 23:25:27 +0500

    feat: create getProvider

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,18 +1,23 @@
 import { ethers } from "ethers";
+import hre from "hardhat";
 import * as dotenv from "dotenv";
+import { IRebalancer } from "../../dist/contracts/typechain/IRebalancer";
 
 dotenv.config();
 
-let provider: ethers.providers.Provider;
-if (process.env.PROVIDER === undefined) throw `PROVIDER is undefined`;
+const getProvider = (): ethers.providers.Provider => {
+    let provider: ethers.providers.Provider;
+    if (process.env.PROVIDER === undefined) throw `PROVIDER is undefined`;
 
-if (process.env.PROVIDER_TYPE == "ipc") {
-    provider = new ethers.providers.IpcProvider(process.env.PROVIDER);
-} else if (process.env.PROVIDER_TYPE == "http") {
-    provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER);
-} else {
-    throw `Unrecognized PROVIDER_TYPE == ${process.env.PROVIDER_TYPE}`;
-}
+    if (process.env.PROVIDER_TYPE == "ipc") {
+        provider = new ethers.providers.IpcProvider(process.env.PROVIDER);
+    } else if (process.env.PROVIDER_TYPE == "http") {
+        provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER);
+    } else {
+        throw `Unrecognized PROVIDER_TYPE == ${process.env.PROVIDER_TYPE}`;
+    }
+    return provider;
+};
 
 async function* getLatestBlock(provider: ethers.providers.Provider) {
     let lastSeenBlockNumber = await provider.getBlockNumber();
